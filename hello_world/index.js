35f7b4bf2a7f7132544050fddbbcb813066aeea4
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-05-10 17:32:33 -0700

    add endpoints for hello and world  and TODO comments

--- hello_world/index.js
+++ hello_world/index.js
@@ -2,7 +2,179 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.send('<p>Enter <strong>/hello</strong> or <strong>/world</strong> in the browser. </p>');
+});
+
+app.get('/hello', function(req, res){
+  // ask OS what time and day it is and return a greeting
+  res.send('Good Morning');
+});
+
+app.get('/hello/monday', function(req, res){
+  // ask the OS what time it is. 
+  // return greeting for Morning if it is 4:00AM - 11:59AM, 
+  // Afternoon if it is 12:00PM - 7:59PM and 
+  // Evening if it is 8:00PM - 3:59AM.
+  res.send('Good Morning');
+});
+
+app.get('/hello/monday/morning', function(req, res){
+  // return a greeting
+  res.send('Good Morning, It\'s Monday!');
+});
+
+app.get('/hello/monday/afternoon', function(req, res){
+  // return a greeting
+  res.send('Good Afternoon, It\'s Tuesday!');
+});
+
+app.get('/hello/monday/evening', function(req, res){
+  // return a greeting
+  res.send('Good Evening, It\'s Tuesday!');
+});
+
+app.get('/hello/tuesday/morning', function(req, res){
+  // return a greeting
+  res.send('Good Morning, It\'s Tuesday!');
+});
+
+app.get('/hello/tuesday/afternoon', function(req, res){
+  // return a greeting
+  res.send('Good Afternoon, It\'s Monday!');
+});
+
+app.get('/hello/tuesday/evening', function(req, res){
+  // return a greeting
+  res.send('Good Evening, It\'s Monday!');
+});
+
+app.get('/hello/tuesday', function(req, res){
+  // ask the OS what time it is. 
+  // return greeting for Morning if it is 4:00AM - 11:59AM, 
+  // Afternoon if it is 12:00PM - 7:59PM and 
+  // Evening if it is 8:00PM - 3:59AM.
+  res.send('Good Morning');
+});
+
+app.get('/hello/wednesday/morning', function(req, res){
+  // return a greeting
+  res.send('Good Morning, It\'s Wednesday!');
+});
+
+app.get('/hello/wednesday/afternoon', function(req, res){
+  // return a greeting
+  res.send('Good Afternoon, It\'s Wednesday!');
+});
+
+app.get('/hello/wednesday/evening', function(req, res){
+  // return a greeting
+  res.send('Good Evening, It\'s Wednesday!');
+});
+
+app.get('/hello/wednesday', function(req, res){
+  // ask the OS what time it is. 
+  // return greeting for Morning if it is 4:00AM - 11:59AM, 
+  // Afternoon if it is 12:00PM - 7:59PM and 
+  // Evening if it is 8:00PM - 3:59AM.
+  res.send('Good Morning');
+});
+
+app.get('/hello/thursday', function(req, res){
+  // ask the OS what time it is. 
+  // return greeting for Morning if it is 4:00AM - 11:59AM, 
+  // Afternoon if it is 12:00PM - 7:59PM and 
+  // Evening if it is 8:00PM - 3:59AM.
+  res.send('Good Morning');
+});
+
+app.get('/hello/thursday/morning', function(req, res){
+  // return a greeting
+  res.send('Good Morning, It\'s Thursday!');
+});
+
+app.get('/hello/thursday/afternoon', function(req, res){
+  // return a greeting
+  res.send('Good Afternoon, It\'s Thursday!');
+});
+
+app.get('/hello/thursday/evening', function(req, res){
+  // return a greeting
+  res.send('Good Evening, It\'s Thursday!');
+});
+
+app.get('/hello/friday', function(req, res){
+  // ask the OS what time it is. 
+  // return greeting for Morning if it is 4:00AM - 11:59AM, 
+  // Afternoon if it is 12:00PM - 7:59PM and 
+  // Evening if it is 8:00PM - 3:59AM.
+  res.send('Good Morning');
+});
+
+app.get('/hello/friday/morning', function(req, res){
+  // return a greeting
+  res.send('Good Morning, It\'s Friday!');
+});
+
+app.get('/hello/friday/afternoon', function(req, res){
+  // return a greeting
+  res.send('Good Afternoon, It\'s Friday!');
+});
+
+app.get('/hello/friday/evening', function(req, res){
+  // return a greeting
+  res.send('Good Evening');;
+});
+
+app.get('/hello/saturday', function(req, res){
+  // ask the OS what time it is. 
+  // return greeting for Morning if it is 4:00AM - 11:59AM, 
+  // Afternoon if it is 12:00PM - 7:59PM and 
+  // Evening if it is 8:00PM - 3:59AM.
+
+  res.send('Good Morning');
+});
+
+app.get('/hello/saturday/morning', function(req, res){
+  // return a greeting
+  res.send('Good Morning, It\'s Saturday!');
+});
+
+app.get('/hello/saturday/afternoon', function(req, res){
+  // return a greeting
+  res.send('Good Afternoon, It\'s Saturday!');
+});
+
+app.get('/hello/saturday/evening', function(req, res){
+  // return a greeting
+  res.send('Good Evening');;
+});
+
+app.get('/hello/sunday', function(req, res){
+  // ask the OS what time it is. 
+  // return greeting for Morning if it is 4:00AM - 11:59AM, 
+  // Afternoon if it is 12:00PM - 7:59PM and 
+  // Evening if it is 8:00PM - 3:59AM.
+  res.send('Good Morning');
+});
+
+app.get('/hello/sunday/morning', function(req, res){
+  // return a greeting
+  res.send('Good Morning, It\'s Sunday!');
+});
+
+app.get('/hello/sunday/afternoon', function(req, res){
+  // return a greeting
+  res.send('Good Afternoon, It\'s Sunday!');
+});
+
+app.get('/hello/sunday/evening', function(req, res){
+  // return a greeting
+  res.send('Good Evening');;
+});
+
+app.get('/world', function(req, res){
+  // return a city
+  res.send('San Jose');
 });
 
 app.listen(3000);
