7c04c93c68d6013c977a3b9b8e1a362cadd2dde6
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-06-28 16:16:09 -0700

    add world component

--- hello_world/client/src/App.js
+++ hello_world/client/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Home from './Home';
 import Hello from './Hello';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import World from './World';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => (
   <Router>
     <Switch>
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/hello" component={Hello} />
+      <Route path='/' exact={true} component={Home} />
+      <Route path='/hello' component={Hello} />
+      <Route path='/world' component={World} />
     </Switch>
   </Router>
 );
