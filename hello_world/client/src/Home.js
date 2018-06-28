aca56b8b6aee8c1c7a4bd4394dd0c10a63c4fd6b
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-06-28 16:16:53 -0700

    add links to other components

--- hello_world/client/src/Home.js
+++ hello_world/client/src/Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
 const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
+  <header>
+    <nav>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/hello'>Hello</Link></li>
+        <li><Link to='/world'>World</Link></li>
+      </ul>
+    </nav>
+  </header>
 );
 export default Home; 
