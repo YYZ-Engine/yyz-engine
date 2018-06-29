aa3578cb7c4d4626bb3cbb98e420a12c838e2dd6
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-06-28 18:52:26 -0700

    remove button, return the greeting immediately instead

--- hello_world/client/src/Hello.js
+++ hello_world/client/src/Hello.js
@@ -2,21 +2,20 @@ import React, { Component } from 'react';
 import App from './App.css';
 
 class Hello extends Component {
-/* TODO - figure out how to return JSON if query includes JSON
+// TODO - figure out how to return JSON if query includes JSON
   constructor(props) {
     super(props);
     this.state = {
-       data: ''
+       greeting: ''
     }
   }
- */
   displayGreeting = () => {
     let greeting = '';  
-    var output = document.getElementById('result');
-    function success(position) {
+    const success = (position) => {
       var d = new Date(position.timestamp);
       var hour = d.getHours();
       var dayOfTheWeek = d.toLocaleString('en-us', {  weekday: 'long' });
+      
       // depending on the hour, set greeting to a specific greeting
       if (hour > 3 && hour < 12) {
         greeting = 'Good Morning ' + dayOfTheWeek;
@@ -25,20 +24,23 @@ class Hello extends Component {
       } else if (hour >= 20 || hour <= 3) {
         greeting = 'Good Evening ' + dayOfTheWeek;
       }
-      output.innerHTML = '<p>' + greeting + '</p>';
+      this.setState({
+        ...this.state, // unwraps the current state
+        greeting: greeting
+      });
+      //output.innerHTML = '<p>' + greeting + '</p>';
     }
 
-    function error() {
+    const error = () => {
       console.log('Unable to retrieve your location');
     }
-    
-    output.innerHTML = 'Loading...';
     
     navigator.geolocation.getCurrentPosition(success, error);
   }
  
   render() {
   /* TODO - figure out how to return JSON if the query includes ?json 
+
   if (props.location.search === '?json') {
     this.displayGreeting().then((resData) => {
       this.setState({
@@ -50,9 +52,8 @@ class Hello extends Component {
     return (
       <div id='result'>
         <p>
-          <button onClick={this.displayGreeting}>
-            Display Greeting
-          </button>
+          {this.displayGreeting()}
+          {this.state.greeting}
         </p>
       </div>
     );
