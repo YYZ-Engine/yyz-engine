fbcedefdd82c892cdbb90437d390614bbadba3e7
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-07-10 22:18:35 -0700

    srender JSON if query includes ?json

--- hello_world/client/src/Hello.js
+++ hello_world/client/src/Hello.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
-import App from './App.css';
+import PropTypes from 'prop-types';
 
 class Hello extends Component {
-// TODO - figure out how to return JSON if query includes JSON
+  static propTypes = {
+    location: PropTypes.object.isRequired
+  }
   constructor(props) {
     super(props);
     this.state = {
-       greeting: ''
+       greeting: '',
+       dayOfWeek: ''
     }
   }
+
   displayGreeting = () => {
-    let greeting = '';  
     const success = (position) => {
+      var greeting = '';
       var d = new Date(position.timestamp);
       var hour = d.getHours();
-      var dayOfTheWeek = d.toLocaleString('en-us', {  weekday: 'long' });
+      var dayOfWeek = d.toLocaleString('en-us', {  weekday: 'long' });
       
       // depending on the hour, set greeting to a specific greeting
       if (hour > 3 && hour < 12) {
-        greeting = 'Good Morning ' + dayOfTheWeek;
+        greeting = 'Good Morning'
       } else if (hour >= 12 && hour < 20) {
-        greeting = 'Good Afternoon ' + dayOfTheWeek;
+        greeting = 'Good Afternoon'
       } else if (hour >= 20 || hour <= 3) {
-        greeting = 'Good Evening ' + dayOfTheWeek;
+        greeting = 'Good Evening'
       }
       this.setState({
         ...this.state, // unwraps the current state
-        greeting: greeting
+        greeting: greeting,
+        dayOfWeek: dayOfWeek
       });
+
     }
 
     const error = () => {
@@ -38,24 +44,22 @@ class Hello extends Component {
   }
  
   render() {
-  /* TODO - figure out how to return JSON if the query includes ?json 
-
-  if (props.location.search === '?json') {
-    this.displayGreeting().then((resData) => {
-      this.setState({
-        data: resData
-      });
-    })
-  }
-   */
-    return (
-      <div id='result'>
-        <p>
-          {this.displayGreeting()}
-          {this.state.greeting}
-        </p>
-      </div>
-    );
+    const {location} = this.props
+    const query = location.search
+    this.displayGreeting()
+    if (query === '?json') {
+      return (
+        JSON.stringify({'greeting': this.state.greeting, 'dayOfWeek': this.state.dayOfWeek})
+      )
+    } else {
+      return (
+        <div id='result'>
+          <p>
+            {this.state.greeting} {this.state.dayOfWeek}
+          </p>
+        </div>
+      );
+    }
   }
 }
 
