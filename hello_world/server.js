72d31ca4163caaaf5ba83c2b09b082a71fced6a7
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-07-10 23:02:57 -0700

    add new route /world and add logic to send JSON if requesting JSON

--- hello_world/server.js
+++ hello_world/server.js
@@ -4,10 +4,34 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// API calls
+let greeting = 'Good Morning';
+let dayOfWeek = 'Monday';
+let city = 'City';
+let state = 'State';
+let country = 'Country';
+
 app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+  if ('json' in req.query) {
+    res.send({ 
+      greeting: greeting, 
+      dayOfWeek: dayOfWeek 
+    });
+  }
+  res.send(greeting + ' ' + dayOfWeek);
 });
+
+
+app.get('/api/world', (req, res) => {
+  if ('json' in req.query) {
+    res.send({ 
+      city: city, 
+      state: state,
+      country: country 
+    });
+  }
+  res.send(city + ', ' + state + ', ' + country);
+});
+
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
