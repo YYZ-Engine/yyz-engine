56fa1ac669eff53f9a6de4253d13ac0cdc32615c
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-07-11 14:34:36 -0700

    add routes for hello/dayoftheweek

--- hello_world/server.js
+++ hello_world/server.js
@@ -34,9 +34,77 @@ app.get('/api/hello', (req, res) => {
     });
   } 
   res.send(greeting + ' ' + dayOfWeek);
-
 });
 
+app.get('/api/hello/monday', (req, res) => {
+  if ('json' in req.query) {
+    res.send({ 
+      greeting: greeting, 
+      dayOfWeek: 'Monday' 
+    });
+  } 
+  res.send(greeting + ' ' + 'Monday');
+});
+
+app.get('/api/hello/tuesday', (req, res) => {
+  if ('json' in req.query) {
+    res.send({ 
+      greeting: greeting, 
+      dayOfWeek: 'Tuesday' 
+    });
+  } 
+  res.send(greeting + ' ' + 'Tuesday');
+});
+
+app.get('/api/hello/wednesday', (req, res) => {
+  if ('json' in req.query) {
+    res.send({ 
+      greeting: greeting, 
+      dayOfWeek: 'Wednesday' 
+    });
+  } 
+  res.send(greeting + ' ' + 'Wednesday');
+});
+
+app.get('/api/hello/thursday', (req, res) => {
+  if ('json' in req.query) {
+    res.send({ 
+      greeting: greeting, 
+      dayOfWeek: 'Thursday' 
+    });
+  } 
+  res.send(greeting + ' ' + 'Thursday');
+});
+
+app.get('/api/hello/friday', (req, res) => {
+  if ('json' in req.query) {
+    res.send({ 
+      greeting: greeting, 
+      dayOfWeek: 'Friday' 
+    });
+  } 
+  res.send(greeting + ' ' + 'Friday');
+});
+
+app.get('/api/hello/saturday', (req, res) => {
+  if ('json' in req.query) {
+    res.send({ 
+      greeting: greeting, 
+      dayOfWeek: 'Saturday' 
+    });
+  } 
+  res.send(greeting + ' ' + 'Saturday');
+});
+
+app.get('/api/hello/sunday', (req, res) => {
+  if ('json' in req.query) {
+    res.send({ 
+      greeting: greeting, 
+      dayOfWeek: 'Sunday' 
+    });
+  } 
+  res.send(greeting + ' ' + 'Sunday');
+});
 
 app.get('/api/world', (req, res) => {
   if ('json' in req.query) {
