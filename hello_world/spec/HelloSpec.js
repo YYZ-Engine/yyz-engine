14cf531b8e581132d9e4291bfc6bc14124ec711b
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-07-19 16:21:33 -0700

    loop through weekdays to test each hello/(day) route

--- hello_world/spec/HelloSpec.js
+++ hello_world/spec/HelloSpec.js
@@ -18,9 +18,24 @@ if (hour > 3 && hour < 12) {
 
 var helloResponse = {'greeting': greeting, 'dayOfWeek': dayOfWeek};
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 it("should respond with a greeting based on local time and day", function(done) {
   request("http://localhost:5000/api/hello?json", function(error, response, body){
     expect(body).toEqual(JSON.stringify(helloResponse));
     done();
   });
+});
+
+it("should respond with a greeting based on local time and given day of week", function(done) {
+  var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  for (var day of days) {
+    request("http://localhost:5000/api/hello/"+day+"?json", function(error, response, body){
+      helloResponse = {'greeting': greeting, 'dayOfWeek': capitalizeFirstLetter(day)};
+      expect(body).toEqual(JSON.stringify(helloResponse));
+      done();
+    });
+  }
 });
