b89a3cb25d592e94a90fbdf6804949a9b42bfe98
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-07-25 17:40:03 -0700

    test for world route

---
+++ hello_world/spec/WorldSpec.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const publicIp = require('public-ip');
+const ip2countrify = require( 'ip2countrify');
+
+let country = '';
+
+publicIp.v6().then(ip => {
+  ip2countrify.lookup(
+    ip,
+    function( ip, results, error ) {
+      if ( error ) {
+        return console.warn( 'An error has occurred: ' + error );
+      }
+      country = results.countryName
+    }
+  );
+});
+var worldResponse = {'country': country};
+
+it("should respond with the user's country", function(done) {
+  request("http://localhost:5000/api/world?json", function(error, response, body){
+    expect(body).toEqual(JSON.stringify(worldResponse));
+    done();
+  });
+});
