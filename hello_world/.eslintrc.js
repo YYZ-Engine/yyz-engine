86e61aeaa6c3bca3b92d424f4a46376eea29399e
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-08-01 15:49:52 -0700

    add globals to ESLint config

--- hello_world/.eslintrc.js
+++ hello_world/.eslintrc.js
@@ -13,7 +13,9 @@ module.exports = {
         "module": false,
         "describe": false,
         "it": false,
-        "expect": false
+        "expect": false,
+        "process": false,
+        "__dirname": false
     },
     "rules": {
         "indent": [
