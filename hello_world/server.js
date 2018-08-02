cbab3d69c7fb8a60b46959ce32cd31009af497d8
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-08-02 16:18:50 -0700

    lint code

--- hello_world/server.js
+++ hello_world/server.js
@@ -5,29 +5,29 @@ const port = process.env.PORT || 5000;
 const yyz_hello = require('./libs/yyz-hello');
 
 app.get('/api/hello', (req, res) => {
-  yyz_hello.getGreeting(req, res);
+	yyz_hello.getGreeting(req, res);
 });
 
 let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 for (var day of days) {
-  app.get('/api/hello/'+day, (req, res) => {
-    yyz_hello.getDayGreeting(req,res);
-  });
+	app.get('/api/hello/'+day, (req, res) => {
+		yyz_hello.getDayGreeting(req,res);
+	});
 }
 
 app.get('/api/world', (req, res) => {
-  yyz_hello.getLocation(req, res);
+	yyz_hello.getLocation(req, res);
 });
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, 'client/build')));
 
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+	// Handle React routing, return all requests to React app
+	app.get('*', function(req, res) {
+		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	});
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- hello_world/spec/WorldSpec.js
+++ hello_world/spec/WorldSpec.js
@@ -4,61 +4,61 @@ const ip2countrify = require('ip2countrify');
 const publicIp = require('public-ip');
 
 describe('get location module', () => {
-  describe('/api/world', () => {
-    var req  = httpMocks.createRequest({
-      method: 'GET',
-      port: '5000',
-      url: '/api/world',
-    });
+	describe('/api/world', () => {
+		var req  = httpMocks.createRequest({
+			method: 'GET',
+			port: '5000',
+			url: '/api/world',
+		});
 
-    var res = httpMocks.createResponse({
-      eventEmitter: require('events').EventEmitter
-    });
+		var res = httpMocks.createResponse({
+			eventEmitter: require('events').EventEmitter
+		});
 
-    yyz_hello.getLocation = (req,res) => {
-      it('get location from IPv6', function( done ) {
-        publicIp.v6().then(ip => {
-          ip2countrify.lookup(
-            ip,
-            function( ip, results ) {
-              var country = results.countryName;
-              expect(country).toBe('United States');
-              done();
-            }
-          );
-        });
-      });
-    }
-    yyz_hello.getLocation(req,res);
-  });
+		yyz_hello.getLocation = (req,res) => {
+			it('get location from IPv6', function( done ) {
+				publicIp.v6().then(ip => {
+					ip2countrify.lookup(
+						ip,
+						function( ip, results ) {
+							var country = results.countryName;
+							expect(country).toBe('United States');
+							done();
+						}
+					);
+				});
+			});
+		};
+		yyz_hello.getLocation(req,res);
+	});
 
-  describe('/api/world?json', () => {
-    var req  = httpMocks.createRequest({
-      method: 'GET',
-      port: '5000',
-      url: '/api/world',
-      query: 'json'
-    });
+	describe('/api/world?json', () => {
+		var req  = httpMocks.createRequest({
+			method: 'GET',
+			port: '5000',
+			url: '/api/world',
+			query: 'json'
+		});
 
-    var res = httpMocks.createResponse({
-      eventEmitter: require('events').EventEmitter
-    });
+		var res = httpMocks.createResponse({
+			eventEmitter: require('events').EventEmitter
+		});
 
-    yyz_hello.getLocation = (req,res) => {
-      it('get location from IPv6', function( done ) {
-        publicIp.v6().then(ip => {
-          ip2countrify.lookup(
-            ip,
-            function( ip, results ) {
-              var response = ({'country': results.countryName});
-              expect(response.country).toBe('United States');
-              done();
-            }
-          );
-        });
-      });
-    }
+		yyz_hello.getLocation = (req,res) => {
+			it('get location from IPv6', function( done ) {
+				publicIp.v6().then(ip => {
+					ip2countrify.lookup(
+						ip,
+						function( ip, results ) {
+							var response = ({'country': results.countryName});
+							expect(response.country).toBe('United States');
+							done();
+						}
+					);
+				});
+			});
+		};
 
-    yyz_hello.getLocation(req,res);
-  });
+		yyz_hello.getLocation(req,res);
+	});
 });
