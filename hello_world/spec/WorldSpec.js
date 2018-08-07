5b4ffa281b66e1a94ea16da48bc4acbeb9a0086a
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-08-06 18:24:32 -0700

    lint code

--- hello_world/spec/WorldSpec.js
+++ hello_world/spec/WorldSpec.js
@@ -1,63 +1,34 @@
-const httpMocks = require('node-mocks-http');
 const ip2countrify = require('ip2countrify');
 const publicIp = require('public-ip');
 
 describe('get location module', () => {
 	describe('/api/world', () => {
-		var req  = httpMocks.createRequest({
-			method: 'GET',
-			port: '5000',
-			url: '/api/world',
-		});
-
-		var res = httpMocks.createResponse({
-			eventEmitter: require('events').EventEmitter
-		});
-
-		const getLocation = (req,res) => {
-			it('get location from IPv6', function( done ) {
-				publicIp.v6().then(ip => {
-					ip2countrify.lookup(
-						ip,
-						function( ip, results ) {
-							var country = results.countryName;
-							expect(country).toBe('United States');
-							done();
-						}
-					);
-				});
+		it('get location from IPv6', function( done ) {
+			publicIp.v6().then(ip => {
+				ip2countrify.lookup(
+					ip,
+					function( ip, results ) {
+						var country = results.countryName;
+						expect(country).toBe('United States');
+						done();
+					}
+				);
 			});
-		};
-		getLocation(req,res);
+		});
 	});
 
 	describe('/api/world?json', () => {
-		var req  = httpMocks.createRequest({
-			method: 'GET',
-			port: '5000',
-			url: '/api/world',
-			query: 'json'
-		});
-
-		var res = httpMocks.createResponse({
-			eventEmitter: require('events').EventEmitter
-		});
-
-		const getLocation = (req,res) => {
-			it('get location from IPv6', function( done ) {
-				publicIp.v6().then(ip => {
-					ip2countrify.lookup(
-						ip,
-						function( ip, results ) {
-							var response = ({'country': results.countryName});
-							expect(response.country).toBe('United States');
-							done();
-						}
-					);
-				});
+		it('get location from IPv6', function( done ) {
+			publicIp.v6().then(ip => {
+				ip2countrify.lookup(
+					ip,
+					function( ip, results ) {
+						var response = ({'country': results.countryName});
+						expect(response.country).toBe('United States');
+						done();
+					}
+				);
 			});
-		};
-
-		getLocation(req,res);
+		});
 	});
 });
