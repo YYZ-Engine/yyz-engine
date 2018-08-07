a9855d730c2ea5a949b4330cf39ae28d9b5beb38
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-08-07 13:59:05 -0700

    update header and intro text

--- hello_world/client/src/App.js
+++ hello_world/client/src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
+import './App.css';
 import Home from './Home';
 import Hello from './Hello';
 import World from './World';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const App = () => (
-  <Router>
-    <Switch>
-      <Route path='/' exact={true} component={Home} />
-      <Route path='/hello' component={Hello} />
-      <Route path='/world' component={World} />
-    </Switch>
-  </Router>
-);
+class App extends Component {
+  render() {
+    return(
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>YYZ Engine Hello World</h1>
+        </header>
+        <div className='App-intro'>
+          <p>Welcome to YYZ Engine Hello World app, where you can get a custom greeting, or get your location.</p>
+        <Router>
+          <Switch>
+            <Route path='/' exact={true} component={Home} />
+            <Route path='/hello' component={Hello} />
+            <Route path='/world' component={World} />
+          </Switch>
+        </Router>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
