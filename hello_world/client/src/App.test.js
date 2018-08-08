79b221a80dcca48e85f139e26d558785a46c23a7
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-08-07 22:18:11 -0700

    move tests for links into Menu

--- hello_world/client/src/App.test.js
+++ hello_world/client/src/App.test.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
-import Home from './Home';
-import Hello from './Hello';
-import World from './World';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const firstDiv = document.createElement('firstDiv');
 
@@ -45,68 +41,4 @@ it('renders title text with className "App-title" without crashing', () => {
   h1.className = 'App-title';
   ReactDOM.render(<App />, h1);
   ReactDOM.unmountComponentAtNode(h1);
-});
-
-const secondDiv = document.createElement('secondDiv');
-
-it('renders second div without crashing', () => {
-  ReactDOM.render(<App />, secondDiv);
-  ReactDOM.unmountComponentAtNode(secondDiv);
-});
-
-it('renders second div with className "App-intro" without crashing', () => {
-  secondDiv.className = 'App-intro';
-  ReactDOM.render(<App />, secondDiv);
-  ReactDOM.unmountComponentAtNode(secondDiv);
-});
-
-it('renders paragraph without crashing', () => {
-  const introParagraph  = document.createElement('introParagraph');
-  ReactDOM.render(<App />, introParagraph);
-  ReactDOM.unmountComponentAtNode(introParagraph);
-});
-
-it('renders paragraph text without crashing', () => {
-  const pText = document.createElement('pText');
-  pText.innerHTML = 'Welcome to YYZ Engine Hello World app, where you can get a custom greeting, or get your location';
-  ReactDOM.render(<App />, pText);
-  ReactDOM.unmountComponentAtNode(pText);
-});
-
-const router = document.createElement('Router');
-const routerChild = document.createElement('Switch');
-
-it('renders Router without crashing', () => {
-  ReactDOM.render(<App />, router);
-  ReactDOM.unmountComponentAtNode(router);
-});
-
-it('renders Switch without crashing', () => {
-  router.appendChild(routerChild);
-  ReactDOM.render(<App />, routerChild);
-  ReactDOM.unmountComponentAtNode(routerChild);
-});
-
-it('renders home route without crashing', () => {
-  const homeRoute = document.createElement('Route');
-  homeRoute.props = { 'path':'/', 'exact':true, 'component':Home };
-  routerChild.appendChild(homeRoute);
-  ReactDOM.render(<App />, homeRoute);
-  ReactDOM.unmountComponentAtNode(homeRoute);
-});
-
-it('renders second route without crashing', () => {
-  const helloRoute = document.createElement('Route');
-  helloRoute.props = { 'path':'/hello', 'component':Hello };
-  routerChild.appendChild(helloRoute);
-  ReactDOM.render(<App />, helloRoute);
-  ReactDOM.unmountComponentAtNode(helloRoute);
-});
-
-it('renders third route without crashing', () => {
-  const worldRoute = document.createElement('Route');
-  worldRoute.props = { 'path':'/world', 'component':World };
-  routerChild.appendChild(worldRoute);
-  ReactDOM.render(<App />, worldRoute);
-  ReactDOM.unmountComponentAtNode(worldRoute);
 });
