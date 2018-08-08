efd0366a4c9af2791b799f2dfbc2858d7141f4b7
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-08-07 22:17:14 -0700

    import Menu

--- hello_world/client/src/App.js
+++ hello_world/client/src/App.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import Home from './Home';
-import Hello from './Hello';
-import World from './World';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Menu from './Menu';
 
 class App extends Component {
   render() {
@@ -12,13 +9,7 @@ class App extends Component {
         <header className='App-header'>
           <h1 className='App-title'>YYZ Engine Hello World</h1>
         </header>
-        <Router>
-          <Switch>
-            <Route path='/' exact={true} component={Home} />
-            <Route path='/hello' component={Hello} />
-            <Route path='/world' component={World} />
-          </Switch>
-        </Router>
+        <Menu/>
       </div>
     );
   }
