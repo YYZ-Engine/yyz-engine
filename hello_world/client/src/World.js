b3233b357b4895c276efa3862dfb4cdb95ed1f9b
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-08-08 17:28:45 -0700

    get country from /api/world and send response to /world

--- hello_world/client/src/World.js
+++ hello_world/client/src/World.js
@@ -1,62 +1,31 @@
 import React, { Component } from 'react';
-import * as Nominatim from "nominatim-browser";
 
 class World extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-       result: ''
-    }
+
+  state = {
+    response: 'Searching for your location...'
+  };
+  componentDidMount() {
+    this.callWorldAPI()
+      .then(res => this.setState({ response: 'You are located in ' + res.country + '.'}))
+      .catch(err => console.log(err));
   }
-
-  findMe = () => {
-    let result = '';
-    let latitude = '';
-    let longitude = ''; 
-
-    result = 'Loading...';
-    const success = (position) => {
-      latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
-      // Use Nominatim, a browser-usable client for Open Street Map's Nominatim service, to look up address based on geocoordinates 
-      Nominatim.reverseGeocode({
-        lat: latitude,
-        lon: longitude,
-        addressdetails: true
-      })
-      .then((data : NominatimResponse) =>
-      {
-        this.setState({result: 'You are located in: ' + data.address.city + ', ' + data.address.state + ', ' + data.address.country});
-      })
-      .catch((error) =>
-      {
-        console.error(error); 
-        result = error;
-      });
-
-    }
-
-    const error = () => {
-      console.log('Unable to retrieve your location');
-      result = 'Unable to retrieve your location';
-    }
-
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
+  callWorldAPI = async () => {
+    const response = await fetch('/api/world?json');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
 
   render() {
-    return (
-      <div>
-        <h1>
-          Hello World
-        </h1>
-        <div id='result'>
-          {this.findMe()}
-          {this.state.result}
+      return (
+        <div className='App-intro'>
+          <p>
+            {this.state.response}
+          </p>
         </div>
-      </div>
-    );
-  }
+      );
+    }
 }
 
 export default World;
