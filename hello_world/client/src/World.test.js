532116db68687fc1196ed797706d541fbf0b1449
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-08-09 15:27:41 -0700

    test /api/world

---
+++ hello_world/client/src/World.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+import getCountry from './api/hello';
+
+jest.mock('axios');
+
+describe('renders response from World API call',() => {
+  test('load a response', () => {
+    const response = {data: [{country: 'United States'}]};
+    axios.get.mockResolvedValue(response);
+    return axios.get('api/world?json').then(data=>{
+      expect(data).toBeDefined()
+      expect(data.country).toEqual(response.data.country)
+    });
+  });
+});
