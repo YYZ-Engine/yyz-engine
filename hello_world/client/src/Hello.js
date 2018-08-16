1820bdfe95089aef6953ea31f0a10117a179d5d7
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-08-16 14:22:57 -0700

    import sun and sky classes

--- hello_world/client/src/Hello.js
+++ hello_world/client/src/Hello.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { getGreeting } from './api/hello';
 import './App.css';
+import './Morning.css';
+import './Afternoon.css';
+import './Evening.css';
 
 class Hello extends Component {
   constructor (props) {
@@ -18,14 +21,31 @@ class Hello extends Component {
   }
 
   render() {
-      return (
-        <div className='App-intro'>
-          <p>
-            {this.state.response}
-          </p>
-        </div>
-      );
+    var sun;
+    var sky;
+    if ((this.state.response.split(' ')[1]) === 'Morning') {
+      sun = 'morningSun';
+      sky = 'morningSky';
+    } else if ((this.state.response.split(' ')[1]) === 'Afternoon') {
+      sun = 'afternoonSun'
+      sky = 'afternoonSky';
+    } else if ((this.state.response.split(' ')[1]) === 'Evening') {
+      sun = 'eveningSun'
+      sky = 'eveningSky';
     }
+
+    return (
+      <div className='App-intro'>
+        <div className={sky}>
+          <div className={sun}>
+          </div>
+          <p> 
+            {this.state.response}
+         </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Hello;
