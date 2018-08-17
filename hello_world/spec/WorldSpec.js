ce08e12207b0dac2d5a2ca933a0898c2ce3c38ca
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-08-16 17:02:26 -0700

    mock /api/world request and test response

--- hello_world/spec/WorldSpec.js
+++ hello_world/spec/WorldSpec.js
@@ -1,34 +1,44 @@
-const ip2countrify = require('ip2countrify');
-const publicIp = require('public-ip');
+const httpMocks = require('node-mocks-http');
+const yyz_hello = require('../libs/yyz-hello');
 
 describe('get location module', () => {
 	describe('/api/world', () => {
-		it('get location from IPv6', function( done ) {
-			publicIp.v6().then(ip => {
-				ip2countrify.lookup(
-					ip,
-					function( ip, results ) {
-						var country = results.countryName;
-						expect(country).toBe('United States');
-						done();
-					}
-				);
-			});
-		});
-	});
+    var req  = httpMocks.createRequest({
+      method: 'GET',
+      url: '/api/world',
+      port: '5000'
+    });
 
+    var res = httpMocks.createResponse({
+      eventEmitter: require('events').EventEmitter
+    });
+
+		it('responds with location', function( done ) {
+      res.on('end', function() {
+        expect(res._getData()).toBeDefined();
+        done();
+      });
+      yyz_hello.getLocation(req,res);
+    });
+	});
 	describe('/api/world?json', () => {
-		it('get location from IPv6', function( done ) {
-			publicIp.v6().then(ip => {
-				ip2countrify.lookup(
-					ip,
-					function( ip, results ) {
-						var response = ({'country': results.countryName});
-						expect(response.country).toBe('United States');
-						done();
-					}
-				);
-			});
-		});
+    var req  = httpMocks.createRequest({
+      method: 'GET',
+      url: '/api/world',
+      query: '?json',
+      port: '5000'
+    });
+
+    var res = httpMocks.createResponse({
+      eventEmitter: require('events').EventEmitter
+    });
+
+		it('responds with location', function( done ) {
+      res.on('end', function() {
+        expect(res._getData()).toBeDefined();
+        done();
+      });
+      yyz_hello.getLocation(req,res);
+    });
 	});
 });
