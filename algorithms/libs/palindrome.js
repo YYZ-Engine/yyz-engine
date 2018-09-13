62581f216ac85fab9d62aeec12812bd491842429
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-09-13 13:08:32 -0700

    refactor palindrome method

--- algorithms/libs/palindrome.js
+++ algorithms/libs/palindrome.js
@@ -1,12 +1,20 @@
 // Write a function that detects if the input is a palindrome or not.
 
-function is_palindrome(string) {
-  // if the string length is even, check if the first half is equal to the second half
-  var halfLength = string.length/2
-  if (string.length % 2 == 0) {
-    return string.substring(0, halfLength) == (string.substring(halfLength, string.length)).split('').reverse().join('');
-  } else {  
-    // if the string length is odd, check if the first half is equal to the second half (not including the middle character)
-    return string.substring(0, Math.floor(halfLength)) == (string.substring((Math.floor(halfLength)+1), string.length)).split('').reverse().join('');
+const is_palindrome = (string) => {
+  var half = string.length/2
+  var length = string.length
+  for (var i=0; i < half; i++) {
+    var first = string[i];
+    var j = string.length
+    var last = string[j-1-i];
+    if (first === last) {
+      return true
+    } else {
+      return false
+    }
   }
+}
+
+module.exports = {
+  is_palindrome: is_palindrome,
 }
