788f40bc9a6856895427c2598456b6f1c51ca2eb
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-09-13 13:09:00 -0700

    add description for tests

--- algorithms/spec/PalindromeSpec.js
+++ algorithms/spec/PalindromeSpec.js
@@ -1,17 +1,21 @@
-const is_palindrome = require('../libs/palindrome');
+const palindrome = require('../libs/palindrome');
 
-it('checks if `mom` is a palindrome', is_palindrome('mom') {
-  expect(is_palindrome('mom')).toBe(true);
-});
-it('checks if `abba` is a palindrome', is_palindrome('abba') {
-  expect(is_palindrome('abba')).toBe(true);
-});
-it('checks if `beereb` is a palindrome', is_palindrome('beereb') {
-  expect(is_palindrome('beereb')).toBe(false);;
-});
-it('checks if `hello olleh` is a palindrome', is_palindrome('hello olleh') {
-  expect(is_palindrome('hello olleh')).toBe(true);
-});
-it('checks if `good morning` is a palindrome', is_palindrome('good morning') {
-  expect(is_palindrome('good morning')).toBe(false);
+describe('checks if input is a palindrome', () => {
+  var string = '';
+  it('checks if "mom" is a palindrome', () => {
+    string = 'mom'
+    expect(palindrome.is_palindrome(string)).toBe(true);
+  });
+  it('checks if "abba" is a palindrome', () => {
+    string = 'abba'
+    expect(palindrome.is_palindrome(string)).toBe(true);
+  });
+  it('checks if "helloworld" is a palindrome', () => {
+    string = 'helloworld'
+    expect(palindrome.is_palindrome(string)).toBe(false);
+  });
+  it('checks if "hello olleh" is a palindrome', () => {
+    string = 'hello olleh'
+    expect(palindrome.is_palindrome(string)).toBe(true);
+  });
 });
