40bde24eb1d5b7bb9c4b25df4f062a8ac09efb6f
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-09-17 15:58:48 -0700

    set up module for reverse method

---
+++ algorithms/libs/reverse.js
@@ -0,0 +1,8 @@
+// Write a method that will take a string as input, and return a new string with the same letters in reverse order.
+
+const reverse = (string) => {
+}
+
+module.exports = {
+  reverse: reverse,
+}
