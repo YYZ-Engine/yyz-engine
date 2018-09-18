ad601d6b1fd59b96c6148a7542daad02867150a8
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-09-17 17:58:10 -0700

    write method for reversing a string

--- algorithms/libs/reverse.js
+++ algorithms/libs/reverse.js
@@ -1,6 +1,11 @@
 // Write a method that will take a string as input, and return a new string with the same letters in reverse order.
 
 const reverse = (string) => {
+  var new_string = '';
+  for (var i = string.length-1; i >= 0; i--) {
+    new_string += string[i]
+  }
+  return new_string
 }
 
 module.exports = {
