34a36d81490aff538cd03b7cb719b027e03f54e3
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-09-17 18:16:34 -0700

    add module for find max and min method

---
+++ algorithms/libs/number.js
@@ -0,0 +1,11 @@
+// Write a function that retrieves the maximum and minimum value of a given array.
+
+const find_max_min = (array) => {
+  var max;
+  var min;
+  return [max, min]
+}
+
+module.exports = {
+  find_max_min: find_max_min,
+}
