fdaad99030a81443c4f8189818fc0ab2d87322dd
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-09-18 16:19:11 -0700

    address #41 - write a method that reverses in place

--- algorithms/libs/reverse.js
+++ algorithms/libs/reverse.js
@@ -2,11 +2,19 @@
 // Do not create a new string
 // Capital letters should remain capital letters
 
+// This works but wouldn't be ideal for long strings 
 const reverse = (string) => {
   if (string === '') return string; 
   return reverse(string.substr(1)) + string[0];
 }
 
+// Write a method that takes an input string, reverses it in place. Don't write a recursive function
+
+const reverse_in_place = (string) => {
+  if (string === '') return string;
+  return string.split('').reverse().join('');
+}
 module.exports = {
-  reverse: reverse
+  reverse: reverse,
+  reverse_in_place: reverse_in_place
 }
