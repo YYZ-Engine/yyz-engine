951a505eccdfd10290875aebb55b73730df69d96
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-09-17 17:33:25 -0700

    remove all punctuation marks before finding longest word

--- algorithms/libs/longest_word.js
+++ algorithms/libs/longest_word.js
@@ -1,7 +1,10 @@
 // Write a method that takes in a string. Return the longest word in the string.
+// Parse the string to that all punctuation marks are removed
+// Make any word with hyphens just one word
 
 const find_longest_word = (string) => {
-  var words = string.split(' ');
+  // remove punctuation marks in the string and split it by one space
+  var words = string.replace(/[^\w\s]|_/g,'').split(' ');
   var longest_word = '';
   for (var i in words) {
     if (longest_word.length <= words[i].length) {
