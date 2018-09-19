8b914d446144166939c27fd99b2798ba45907f60
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-09-18 18:03:14 -0700

    fix syntax issues

--- algorithms/spec/ArraySpec.js
+++ algorithms/spec/ArraySpec.js
@@ -1,17 +1,17 @@
-const array = require('../libs/array');
+const arrays = require('../libs/arrays');
 
 describe('find_duplicate_values method', () => {
-  var array = [];
+  var arr = [];
   it('checks if [10,0] are the duplicate values in [10,0,-5,10,b,0,a]', () => {
-    array = [10,0,-5,10,b,0,a]
-    expect(array.find_duplicate_values(array)).toBe([10, 0]);
+    arr = [10,0,-5,10,'b',0,'a']
+    expect(arrays.find_duplicate_values(arr)).toBe([10, 0]);
   });
   it('checks if [d,o,g] are the duplicate values in [d,2,4,o,6,d,o,g,9,g,+]', () => {
-    array = [d,2,4,o,6,d,o,g,9,g,+]
-    expect(array.find_duplicate_values(array)).toBe([d, o, g]);
+    arr = ['d',2,4,'o',6,'d','o','g',9,'g','+']
+    expect(arrays.find_duplicate_values(arr)).toBe(['d', 'o', 'g']);
   });
   it('checks if [C,a,b] are the duplicate values in [d,2,4,o,6,C,a,9,C,c,b,a,g,b,+]', () => {
-    array = [d,2,4,o,6,C,a,9,C,c,b,a,g,b,+]
-    expect(array.find_duplicate_values(array)).toBe([C, a, b]);
+    arr = ['d',2,4,'o',6,'C','a',9,'C','c','b','a','g','b','+']
+    expect(arrays.find_duplicate_values(arr)).toBe(['C', 'a', 'b']);
   }); 
 });
