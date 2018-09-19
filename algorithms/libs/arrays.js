8b138854f7da9f3fb8d73940cef7220121c22f4a
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-09-18 18:03:24 -0700

    rename file

---
+++ algorithms/libs/arrays.js
@@ -0,0 +1,11 @@
+// Write a function that finds duplicate values of a given array.
+
+const find_duplicate_values = (array) => {
+  values = [];
+  
+  return values;
+}
+
+module.exports = {
+  find_duplicate_values: find_duplicate_values
+}
