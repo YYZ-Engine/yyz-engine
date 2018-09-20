9d62c58578392258c1e151282b39be0a448c09ee
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-09-20 14:50:40 -0700

    write method for finding factorial of number. address issue #33

--- algorithms/libs/number.js
+++ algorithms/libs/number.js
@@ -27,8 +27,22 @@ const find_max_min = (array) => {
   return [max, min]
 }
 
+// Write a function that takes an integer n in; it should return n*(n-1)(n-2)...21. Assume n >= 0.
+// As a special case, factorial(0) == 1.
+
+const factorial = (number) => {
+  if (number === 0 || number === 1) return 1;
+  var result = 1;
+  while ( number > 1) {
+     result *= number
+     number -- 
+  }
+  return result;
+}
+
 module.exports = {
   find_max: find_max,
   find_min: find_min,
   find_max_min: find_max_min,
+  factorial: factorial
 }
