22e43621d7b7f7627299c29683e503af67c76fca
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-09-24 14:31:36 -0700

    write method for finding duplicate values in an array

--- algorithms/libs/arrays.js
+++ algorithms/libs/arrays.js
@@ -1,9 +1,33 @@
 // Write a function that finds duplicate values of a given array.
 
 const find_duplicate_values = (array) => {
-  values = [];
-  
-  return values;
+  var dup_values = {};
+  var result = [];
+  const isNumeric = (n) => { 
+    return !isNaN(parseFloat(n)) && isFinite(n); 
+  }
+
+  // assign the count of array element to dup_values[element] 
+  for (var element of array) {
+    // if the value is a number, increment the count and assign it to the key
+    if (isNumeric(dup_values[element])) { 
+      dup_values[element] ++; 
+    } else { // if the value is not a number, assign key to a value of 1
+      dup_values[element] = 1; 
+    }
+  }
+
+  // find the values greater than 1 and push to the result array
+  for (var prop in dup_values) {
+    if (dup_values[prop] > 1) {
+      if (isNumeric(prop)) { 
+        result.push(parseFloat(prop))
+      } else {
+        result.push(prop)
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
