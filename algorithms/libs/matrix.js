0ec8b75324262382f3ae2caafc5894ebf55af6da
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-09-24 17:05:41 -0700

    add method for creating a matrix given x and y coordinates

--- algorithms/libs/matrix.js
+++ algorithms/libs/matrix.js
@@ -6,6 +6,22 @@ const get_random_character = () => {
   return randomChar
 }
 
+/*
+Creates a randomly generated matrix of single digit hexadecimal
+characters (as strings) by passing x and y coordinates of the matrix.
+*/
+const matrix_create = (x,y) => {
+  var result = [];
+  var x_coord = 0;
+  var y_coord = 0;
+  while (x_coord < x && y_coord < y) {
+    result.push([get_random_character(), get_random_character()]);
+    y_coord ++
+    x_coord ++
+  }
+  return result;
+}
+
 // Takes a matrix as argument an switches the x and y axis on it. 
 const matrix_flip = (arr) => {
   return [[arr[0][0], arr[1][0]], [arr[0][1], arr[1][1]]];
