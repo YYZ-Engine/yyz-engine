291341b6b05bf0de62ea8a1340146f4930003417
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-09-25 17:13:13 -0700

    fix syntax issue

--- algorithms/spec/MatrixSpec.js
+++ algorithms/spec/MatrixSpec.js
@@ -8,12 +8,22 @@ describe('matrix_create', () => {
   });
 });
 
-describe('matrix_flip', () => {
+describe('matrix_transpose', () => {
   var arr = [];
   var result = [];
-  it('checks if [["a", "1"], ["b", "2"]] is [["a", "b"], ["1", "2"]] flipped', () => {
+  it('checks if [["a", "1"], ["b", "2"]] is [["a", "b"], ["1", "2"]] transposed', () => {
     arr = [["a", "b"], ["1", "2"]];
-    result = [["a", "1"], ["b", "2"]]
-    expect(matrix.matrix_flip(arr)).toEqual(result);
+    result = [["a", "1"], ["b", "2"]];
+    expect(matrix.matrix_transpose(arr)).toEqual(result);
+  });
+  it('checks if [["1", "2", "3"], ["4","5","6"], ["7", "8", "9"]] is [["1","4","7"], ["2","5","8"], ["3","6","9"]] transposed', () => {
+    arr = [["1", "2", "3"], ["4","5","6"], ["7", "8", "9"]];
+    result = [["1","4","7"],["2","5","8"],["3","6","9"]];
+    expect(matrix.matrix_transpose(arr)).toEqual(result);
+  });
+  it('checks if [["1", "2", "3"], ["4","5","6"]] is [["1","4"],["2","5"],["3","6"]] transposed', () => {
+    arr = [["1", "2", "3"], ["4","5","6"]];
+    result = [["1","4"],["2","5"],["3","6"]];
+    expect(matrix.matrix_transpose(arr)).toEqual(result);
   });
 });
