b085358e19b07cee0f894478eb565b55e1d8d3a4
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-10-06 16:07:11 -0700

    rewrite test to instantiate Nodes correctly

--- algorithms/spec/LinkedListSpec.js
+++ algorithms/spec/LinkedListSpec.js
@@ -1,25 +1,28 @@
 const linked_list = require('../libs/linked_list');
 
 describe('Singly Linked List', () => {
-  var singlyLinkedList = new linked_list.SinglyLinkedList();
-  var result = singlyLinkedList;
-  result.append(5);
-  result.append(10);
-  result.append(15);
+  var list = new linked_list.SinglyLinkedList();
+  list.append(5);
+  list.append(10);
+  list.append(15);
   it('checks if you can append new node to the linked list', () => {
-    expect(result.length).toEqual(3);
-    expect(result.head.data).toEqual(5);
-    expect(result.head.next.data).toEqual(10);
-    expect(result.head.next.next.data).toEqual(15);
+    expect(list.length).toEqual(3);
+    expect(list.head.data).toEqual(5);
+    expect(list.head.next.data).toEqual(10);
+    expect(list.head.next.next.data).toEqual(15);
   });
   it('checks if you can collect all the node data and return it in an Array', () => { 
-    expect(result.collect()).toEqual([5, 10, 15]);
+    expect(list.collect()).toEqual([5, 10, 15]);
   });
   it('checks if you can find a node by its index in the list', () => {
-    var Node = new linked_list.Node(); 
-    expect(result.findByIndex(0)).toEqual(Node({ data: 5, next: Node({ data: 10, next: Node({ data: 15, next: null }) }) }));
+    var node1 = new linked_list.Node(5);
+    var node2 = new linked_list.Node(10);
+    var node3 = new linked_list.Node(15);
+    node1.next = node2;
+    node2.next = node3;
+    expect(list.findByIndex(0)).toEqual(node1);
   });
   it('checks if you can find a node by its index in the list', () => {
-    expect(result.findByIndex(4)).toEqual(-1);
+    expect(list.findByIndex(4)).toEqual(-1);
   });
 });
