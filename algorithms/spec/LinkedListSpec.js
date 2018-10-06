9c02bb2672222013b2a2ed7bf60ebd1efc57a22b
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-10-05 17:13:57 -0700

    fix syntax error

--- algorithms/spec/LinkedListSpec.js
+++ algorithms/spec/LinkedListSpec.js
@@ -3,8 +3,10 @@ const linked_list = require('../libs/linked_list');
 describe('Singly Linked List', () => {
   var singlyLinkedList = new linked_list.SinglyLinkedList();
   var result = singlyLinkedList;
-  it('checks if you can add a new node to the top of the linked list', () => {
-    result = singlyLinkedList.create(5);
-    expect(result.length.toEqual(1));
+  it('checks if you can append new node to the top of the linked list', () => {
+    result.append(5);
+    result.append(10);
+    result.append(15);
+    expect(result.length).toEqual(3);
   });
 });
