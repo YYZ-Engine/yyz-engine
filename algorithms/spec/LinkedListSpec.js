f30059e32f3f0a0e0e573d9f5e068701fe9cd719
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-10-06 15:31:56 -0700

    add test for collecting all node data

--- algorithms/spec/LinkedListSpec.js
+++ algorithms/spec/LinkedListSpec.js
@@ -3,13 +3,17 @@ const linked_list = require('../libs/linked_list');
 describe('Singly Linked List', () => {
   var singlyLinkedList = new linked_list.SinglyLinkedList();
   var result = singlyLinkedList;
-  it('checks if you can append new node to the top of the linked list', () => {
-    result.append(5);
-    result.append(10);
-    result.append(15);
+  result.append(5);
+  result.append(10);
+  result.append(15);
+  it('checks if you can append new node to the linked list', () => {
     expect(result.length).toEqual(3);
     expect(result.head.data).toEqual(5);
     expect(result.head.next.data).toEqual(10);
     expect(result.head.next.next.data).toEqual(15);
   });
+  it('checks if you can collect all the node data and return it in an Array', () => { 
+    expect(result.collect()).toEqual([5, 10, 15]);
+  });
+
 });
