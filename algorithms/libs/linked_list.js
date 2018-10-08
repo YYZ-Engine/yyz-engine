6309ef3b780914abe622bd7b584b2a3c943da59b
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-10-07 21:52:49 -0700

    add method to remove node from list

--- algorithms/libs/linked_list.js
+++ algorithms/libs/linked_list.js
@@ -10,7 +10,7 @@ Implement the following functions for your Node class:
  -  append a new node to linked list
  -  collect all node data and return it in a javascript Array
  -  find a node by its index in the list (return -1 when no Node exists)
- -  delete a Node in a list and reconnect the list items so that the linked list is not corrupted
+ -  remove a Node in a list and reconnect the list items so that the linked list is not corrupted
 
 */
 
@@ -84,6 +84,17 @@ function SinglyLinkedList() {
       count++;
     }
     return nodeToCheck;
+  };
+  
+  // remove a Node in a list and reconnect the list items so that the linked list is not corrupted
+  this.remove = function(index) {
+     if (index > this.length) return -1;
+
+     var nodeToCheck = this.head, 
+          count = 0,
+          prevNode = null;
+
+    if(index === 0) return nodeToCheck.next;
   };
 };
 
