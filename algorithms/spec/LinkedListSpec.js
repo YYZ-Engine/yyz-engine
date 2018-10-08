67ce43cfcd2bc319bd61dd833d5c44e6cc5e5429
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-10-07 23:03:22 -0700

    add test to check if removed node is removed from memory

--- algorithms/spec/LinkedListSpec.js
+++ algorithms/spec/LinkedListSpec.js
@@ -75,4 +75,18 @@ describe('Singly Linked List', () => {
     expect(list.remove(2)).toEqual(node1);
     expect(list.length).toEqual(2);
   });
+  it('checks if the removed node is no longer in memory', () => {
+    var list = new linked_list.SinglyLinkedList();
+    list.append(5);
+    list.append(10);
+    list.append(15);
+    var node1 = new linked_list.Node(5);
+    var node2 = new linked_list.Node(10);
+    var node3 = new linked_list.Node(15);
+    node1.next = node2;
+    expect(list.remove(2)).toEqual(node1);
+    expect(list.findByIndex(3)).toEqual(-1);
+    expect(list.length).toEqual(2);
+  });
+
 });
