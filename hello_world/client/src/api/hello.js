8a46ea737120d475589cf48cb7cd9bcbd0bb77ff
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-10-09 16:36:08 -0700

    address issue #50 - move hello_world into a new repository

--- hello_world/client/src/api/hello.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const getGreeting  = async () => {
-	const response = await axios.get('api/hello?json');
-	if (response.status !== 200) throw Error(response.data.message);
-	return response;
-};
-
-export { getGreeting };
--- hello_world/client/src/api/world.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const getCountry = async () => {
-	const response = await axios.get('api/world?json');
-	if (response.status !== 200) throw Error(response.data.message);
-	return response;
-};
-
-export { getCountry };
